de987d32741a1dd86fa241e6b1b9ed1a205e82c7
David.Gao <david.alpha.fox@gmail.com>
2018-10-17 17:01:57 +0800

    we need a gun to fire

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 PROJECT = ai_npm
 PROJECT_DESCRIPTION = A private npm registry and local cache
 PROJECT_VERSION = 0.1.0
-DEPS = cowboy ailib
+DEPS = cowboy gun ailib 
 dep_cowboy_commit = 2.5.0
+dep_gun_commit = 1.3.0
 dep_ailib = git https://github.com/DavidAlphaFox/ailib.git master
 include erlang.mk
---
+++ src/ai_npm.erl
@@ -0,0 +1,18 @@
+-module(ai_npm).
+
+%% API
+-export([start/0]).
+
+start() ->
+    ok = application:start(crypto),
+    ok = application:start(asn1),
+    ok = application:start(public_key),
+    ok = application:start(ssl),
+    ok = application:start(ranch),
+    ok = application:start(cowlib),
+    ok = application:start(cowboy),
+		ok = application:start(gun),		
+		ok = application:start(poolboy),
+    ok = application:start(sasl),
+    ok = application:start(ailib),
+		ok = application:start(ai_npm).
