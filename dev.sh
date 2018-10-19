b27c8569316a223b3d706ea077bbffad1d81289c
David.Gao <david.alpha.fox@gmail.com>
2018-10-19 16:38:05 +0800

    meta fetch and cache with idempotence task

--- dev.sh
+++ dev.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $(pwd)/ebin $(find $(pwd)/deps -type d -name ebin | xargs) -s ai_npm
+export ERL_MAX_PORTS=2048
+exec erl  -pa $(pwd)/ebin $(find $(pwd)/deps -type d -name ebin | xargs) -s ai_npm
