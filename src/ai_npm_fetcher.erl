6d8b21991c686db261146f22f4e890a60e27a794
David.Gao <david.alpha.fox@gmail.com>
2018-10-22 20:28:23 +0800

    proxy tarball

--- src/ai_npm_fetcher.erl
+++ src/ai_npm_fetcher.erl
@@ -1,5 +1,5 @@
 -module(ai_npm_fetcher).
--export([fetch_package/1]).
+-export([fetch_package/1,fetch_tarball/1]).
 
 fetch_package(Ctx)->
     {ok,ConnPid} = ai_npm_gun:open(Ctx),
@@ -18,6 +18,88 @@ fetch_package(Ctx)->
             CacheProcessor(Status,Headers,Body2)
     end.
 
+fetch_tarball(Ctx)->
+	{ok,ConnPid} = ai_npm_gun:open(Ctx),
+  {ok, _Protocol} = gun:await_up(ConnPid),
+  Url = proplists:get_value(url,Ctx),
+	Tarball = erlang:binary_to_list(proplists:get_value(tarball,Ctx)),
+	ok = ai_file:create_priv_dir(ai_npm,"storage"),
+	ok = ai_file:create_priv_dir(ai_npm,"tmp"),
+	DefaultStorage = ai_file:priv_dir(ai_npm,"storage"),
+	TmpDir = ai_file:priv_dir(ai_npm,"tmp"),
+	Storage = proplists:get_value(storage,Ctx,DefaultStorage),
+	ReqHeaders =  ai_npm_gun:headers(proplists:get_value(headers,Ctx,[]),Ctx),
+  StreamRef = gun:get(ConnPid, Url,ReqHeaders),
+	Final = fun(Status,Headers,TmpFile,FinalFile)->
+						io:format("final ~p ~p~n",[TmpFile,FinalFile]),
+						case filelib:ensure_dir(FinalFile) of
+							ok ->
+									case file:rename(TmpFile, FinalFile) of
+															ok ->
+																io:format("rename ~p~n",[FinalFile]), 
+																{data,Status,Headers,FinalFile};
+															Error -> 
+																io:format("rename error: ~p~n",[Error]),
+																Error
+										end;
+							DirError -> DirError
+						end
+	end,
+	Downloader = fun(Status,Headers,Dir)->
+								TmpFile = filename:join([Dir,Tarball]),
+								FinalFile = filename:join([Storage,Tarball]),
+								case ai_file:open_for_write(TmpFile) of
+													{ok,Fd} ->
+														io:format("open file ~p~n",[TmpFile]),
+														case download_tarball(Fd,ConnPid,StreamRef) of
+															{ok,StreamRef} -> 
+																		Final(Status,Headers,TmpFile,FinalFile);
+															{error,StreamRef,Reason} ->
+																		io:format("error occur: ~p~n",[Reason]),
+																		{error,Reason}
+														end;
+											Error -> 
+														io:format("open file error: ~p~n",[Error]),
+														Error
+									end
+							 end,
+    io:format("start to fetch tarball ~p~n",[Url]),
+		case gun:await(ConnPid, StreamRef) of
+        {response, fin, Status, Headers} ->
+						io:format("no_data ~p ~p ~p~n",[Url,Status,Headers]),
+					{no_data,Status,Headers};
+        {response, nofin, Status,Headers} ->
+					io:format("data ~p ~p ~p~n",[Url,Status,Headers]),
+					ai_tmp:run_with_tmp("ai_npm_downloader",[{remove,true},{path,TmpDir}],{Downloader,[Status,Headers]})
+    end.
 
 
+download_tarball(Fd,ConnPid,StreamRef)->
+		MRef = erlang:monitor(process,ConnPid),
+		try 
+			download_tarball(Fd,ConnPid,StreamRef,MRef)
+		catch 
+			_Error: Reason-> {error,StreamRef,Reason}
+		after
+			erlang:demonitor(MRef)
+		end.
 
+download_tarball(OFile,Socket,StreamRef,MRef)->
+	receive
+					{gun_data, Socket, StreamRef, nofin, Data} ->
+							io:format("got data~n"),
+							ok = file:write(OFile, Data),
+							download_tarball(OFile, Socket, StreamRef,MRef);
+					{gun_data, Socket, StreamRef, fin, Data} ->
+							ok = file:write(OFile, Data),
+							ok = file:sync(OFile),
+							ok = file:close(OFile),
+					io:format("finish write~n"),
+							{ok,StreamRef};
+					{'DOWN', MRef, process, Socket, Reason} ->
+						{error,StreamRef,Reason}
+	after 10000 ->
+			gun:close(Socket),
+			{error,StreamRef,timeout}
+	end.
+		
--- src/ai_npm_tarball_handler.erl
+++ src/ai_npm_tarball_handler.erl
@@ -1,6 +1,61 @@
 -module(ai_npm_tarball_handler).
 -include("ai_npm.hrl").
-
+-define(RESPONSE_HEADERS, [
+                           {<<"Content-Type">>, <<"application/octet-stream">>}
+                          ]).
 -export([init/2]).
 init(Req,State)->
-		{Req,State}.
+    Req2 = case fetch_with_cache(Req) of
+               {data,ResHeaders,Data}->
+                   cowboy_req:reply(200,clean_headers(ResHeaders),Data,Req);
+               {data,Status,ResHeaders,Data}->
+                   cowboy_req:reply(Status,clean_headers(ResHeaders),Data,Req);
+               {no_data,Status,ResHeaders}->
+                   cowboy_req:reply(Status,clean_headers(ResHeaders),Req);
+               not_found ->
+                   Body = <<"Internal Server Error">>,
+                   cowboy_req:reply(500, maps:from_list(?RESPONSE_HEADERS), Body, Req)
+           end,
+    {ok, Req2, State}.
+fetch_with_cache(Req) ->
+    Tarball = cowboy_req:binding(tarball,Req),
+    Path = cowboy_req:path(Req),
+    Headers = cowboy_req:headers(Req),
+    case ai_npm_mnesia_cache:try_hit_cache(Path) of
+        not_found ->
+            fetch_without_cache(Path,maps:to_list(Headers),Tarball);
+        {expired,C}->
+            NewHeaders = [{<<"if-none-match">>,C#cache.etag} , {<<"if-modified-since">>,C#cache.last_modified}] ++ maps:to_list(Headers),
+            fetch_without_cache(Path,NewHeaders,Tarball);
+        {ok,C} ->
+            File = C#cache.cache_key,
+            ResHeaders = C#cache.headers,
+            {ok,Size} = ai_file:file_size(File),
+            {data,ResHeaders,{sendfile,0,Size,File}}
+    end.
+fetch_without_cache(Path,Headers,Tarball) ->
+    Ctx = [{url,Path},{headers,Headers},{tarball,Tarball}],
+    case ai_idempotence_pool:task_add(pkg,Path,{ai_npm_fetcher,fetch_tarball,[Ctx]}) of
+        {done,{data,Status,ResHeaders,File}} -> 
+            {ok,Size} = ai_file:file_size(File),
+            {data,Status,ResHeaders,{sendfile,0,Size,File}};
+        {done,{no_data,Status,ResHeaders}} -> 
+            if 
+                Status == 304 ->
+                    ai_npm_mnesia_cache:refresh_headers(Path,ResHeaders),
+                    C = ai_npm_mnesia_cache:hit_cache(Path),
+                    File = C#cache.cache_key,
+                    ResHeaders = C#cache.headers,
+                    {ok,Size} = ai_file:file_size(File),
+                    {data,ResHeaders,{sendfile,0,Size,File}};
+                true ->
+                    {no_data,Status,ResHeaders}
+            end;
+        _ -> not_found
+    end.
+
+clean_headers(Headers)-> 
+    Exclued = [<<"set-cookie">>,<<"etag">>,<<"last-modified">>,<<"content-encoding">>,
+               <<"cf-cache-status">>,<<"accept-ranges">>,<<"cf-ray">>,<<"expect-ct">>],
+    NewHeaders = [{<<"content-encoding">>,<<"identity">>} | lists:filter(fun({Key,_V})-> not lists:member(Key,Exclued) end,Headers)],
+    maps:from_list(NewHeaders).
