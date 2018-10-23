b17fe10c720ab5efccc545831c98ce49e1773ab2
David.Gao <david.alpha.fox@gmail.com>
2018-10-23 17:10:26 +0800

    update app file

--- src/ai_npm_app.erl
+++ src/ai_npm_app.erl
@@ -7,6 +7,23 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
+    application:ensure_start(crypto),
+    application:ensure_start(asn1),
+    application:ensure_start(public_key),
+    application:ensure_start(ssl),
+    application:ensure_start(inets),
+    application:ensure_start(syntax_tools),
+    application:ensure_start(compiler),
+    application:ensure_start(edoc),
+    application:ensure_start(ranch),
+    application:ensure_start(cowlib),
+    application:ensure_start(cowboy),
+		application:ensure_start(gun),		
+		application:ensure_start(poolboy),
+    application:ensure_start(sasl),
+    application:ensure_start(ailib),
+    application:ensure_start(urilib),
+    application:ensure_start(jsx),
     ai_npm_storage:ensure_default_storage(),
     Result = ai_npm_sup:start_link(),
     ai_mnesia:ensure(fun()-> create_db() end),
