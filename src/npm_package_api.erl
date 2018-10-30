c067e48cdf5c1c35563dad4193e8e867f36d5fa2
David.Gao <david.alpha.fox@gmail.com>
2018-10-30 15:31:34 +0800

    fetcher headers must be proplists

--- src/npm_package_api.erl
+++ src/npm_package_api.erl
@@ -95,7 +95,7 @@ fetch_with_cache(Req)->
     case ai_http_cache:validate_hit(Url) of 
         not_found -> 
             io:format("Cache: not_found: ~p~n",[ScopeName]),
-            fetch_without_cache(Url,Headers,ServerName,ScopeName,Version);
+            fetch_without_cache(Url,maps:to_list(Headers),ServerName,ScopeName,Version);
         {expired,Etag,Modified} -> 
             io:format("Cache: expired: ~p~n",[ScopeName]),
             NewHeaders = npm_req:req_headers(Etag,Modified,Headers),
