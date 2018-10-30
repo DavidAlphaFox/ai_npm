fdc0c84784f2a8d532345c393149fe85b58d17df
David.Gao <david.alpha.fox@gmail.com>
2018-10-30 15:24:32 +0800

    package process

--- src/npm_package_api.erl
+++ src/npm_package_api.erl
@@ -57,7 +57,6 @@ req(?PUT,Req,State)->
 	{ok,Req1,State};	
 
 req(?GET,Req,State)->
-    io:format("process req:~p~n",[Req]),
     Req2 = case fetch_with_private(Req) of
                {data,ResHeaders,Data} ->
                    cowboy_req:reply(200,npm_req:res_headers(ResHeaders),Data,Req);
@@ -94,11 +93,15 @@ fetch_with_cache(Req)->
     Url = cowboy_req:path(Req),
     Headers = cowboy_req:headers(Req),
     case ai_http_cache:validate_hit(Url) of 
-        not_found -> fetch_without_cache(Url,maps:to_list(Headers),ServerName,ScopeName,Version);
+        not_found -> 
+            io:format("Cache: not_found: ~p~n",[ScopeName]),
+            fetch_without_cache(Url,Headers,ServerName,ScopeName,Version);
         {expired,Etag,Modified} -> 
-            NewHeaders = npm_req:req_headers(Etag,Modified,maps:to_list(Headers)),
+            io:format("Cache: expired: ~p~n",[ScopeName]),
+            NewHeaders = npm_req:req_headers(Etag,Modified,Headers),
             fetch_without_cache(Url,NewHeaders,ServerName,ScopeName,Version);
         {hit,CacheKey,ResHeaders}->
+            io:format("Cache: hit: ~p~n",[ScopeName]),
             reply(Url,ResHeaders,ServerName,CacheKey,Version)
     end.
 
