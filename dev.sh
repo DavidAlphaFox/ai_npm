f6623a9e7b93a38aac93fadf00eb05fea51445e0
David.Gao <david.alpha.fox@gmail.com>
2018-11-01 13:15:27 +0800

    use kernel poll default

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd `dirname $0`
 export ERL_MAX_PORTS=2048
-exec erl  -pa $(pwd)/ebin $(find $(pwd)/deps -type d -name ebin | xargs) -s ai_npm
+exec erl +K true -pa $(pwd)/ebin $(find $(pwd)/deps -type d -name ebin | xargs) -s ai_npm
