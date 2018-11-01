c9c9199be356ace4269a17790c3b8724ababf153
David.Gao <david.alpha.fox@gmail.com>
2018-11-01 12:35:26 +0800

    unknown npm api

--- src/npm_package_api.erl
+++ src/npm_package_api.erl
@@ -96,14 +96,14 @@ fetch_with_cache(Req)->
     Headers = cowboy_req:headers(Req),
     case ai_http_cache:validate_hit(Url) of 
         not_found -> 
-            io:format("Cache: [not_found] ~p~n",[ScopeName]),
+            io:format("Cache: [not_found] ~p~n",[Url]),
             fetch_without_cache(Url,maps:to_list(Headers),ServerName,ScopeName,Version);
         {expired,Etag,Modified} -> 
-            io:format("Cache: [expired] ~p~n",[ScopeName]),
+            io:format("Cache: [expired] ~p~n",[Url]),
             NewHeaders = npm_req:req_headers(Etag,Modified,Headers),
             fetch_without_cache(Url,NewHeaders,ServerName,ScopeName,Version);
         {hit,CacheKey,ResHeaders}->
-            io:format("Cache: [hit] ~p~n",[ScopeName]),
+            io:format("Cache: [hit] ~p~n",[Url]),
             reply(Url,ResHeaders,ServerName,CacheKey,Version)
     end.
 
