aef8cca2d8ba17466bf2367a986a5cc64e46f5a3
David.Gao <david.alpha.fox@gmail.com>
2018-11-01 13:03:40 +0800

    more tarball fetcher

--- src/ai_npm_sup.erl
+++ src/ai_npm_sup.erl
@@ -19,7 +19,7 @@ default_woker_pool()->
        {npm_package_running_pool,
         [{size,10},{worker_module,npm_package_task_worker},{strategy,fifo}],[]},
        {npm_tarball_running_pool,
-        [{size,10},{worker_module,npm_tarball_task_worker},{strategy,fifo}],[]}
+        [{size,20},{worker_module,npm_tarball_task_worker},{strategy,fifo}],[]}
       ].
 worker_pool_specs()->
     lists:map(fun({Name, Args, WorkerArgs}) ->
